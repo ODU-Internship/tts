0ad136e84f5e7477f61adec363df947ffa029704
Likith <likithsrinath2000@gmail.com>
2021-05-09 12:15:43 +0530

    Add Supervisor Routes

---
+++ src/pages/Supervisor/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-indent */
+/** @jsxImportSource @emotion/react */
+
+import { Flex } from '@chakra-ui/react';
+
+const Home = () => (
+    <Flex justifyContent="center" fontSize="2xl">
+        <p>This is Supervisor dashboard</p>
+    </Flex>
+);
+
+export default Home;
--- src/pages/Supervisor/Supervisor.js
+++ src/pages/Supervisor/Supervisor.js
@@ -1,9 +1,18 @@
 /** @jsxImportSource @emotion/react */
 
-const Supervisor = () => (
-  <div>
-    <p>Hello Supervisor</p>
-  </div>
-);
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import Login from './Login/Login';
+import SupervisorRoute from './SupervisorRoute/SupervisorRoute';
+import Dashboard from './Dashboard/Dashboard';
+
+const Supervisor = () => {
+  const { path } = useRouteMatch();
+  return (
+    <Switch>
+      <Route path={`${path}/login`} component={Login} />
+      <SupervisorRoute path={path} component={Dashboard} />
+    </Switch>
+  );
+};
 
 export default Supervisor;
