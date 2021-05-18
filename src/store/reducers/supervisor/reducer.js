0925238c8553055328c3daa74389c35c1da4a3cd
Jeevan Surendran <jvns67@gmail.com>
2021-05-18 16:24:54 +0530

    Make reload feel better

--- src/store/reducers/supervisor/reducer.js
+++ src/store/reducers/supervisor/reducer.js
@@ -1,11 +1,19 @@
 import { UPDATE_SUPERVISOR_USER, UPDATE_SUPERVISOR_MESSAGES } from '../../actions';
 
-const supervisorReducer = (state = { messages: [] }, action) => {
+const supervisorReducer = (state = { messages: { } }, action) => {
   switch (action.type) {
     case UPDATE_SUPERVISOR_USER:
       return { ...state, user: action.user };
     case UPDATE_SUPERVISOR_MESSAGES:
-      return { ...state, messages: action.messages };
+      return {
+        ...state,
+        messages: {
+          ...(action.messages.reduce(
+            (acc, { _id, ...message }) => ({ [_id]: message, ...acc }),
+            {},
+          )),
+        },
+      };
     default:
       return state;
   }
